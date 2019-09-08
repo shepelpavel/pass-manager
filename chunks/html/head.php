2b06f5541d479efb80208f8d50e8d1810f7aa32a
Shepel Pavel <shepelpav@gmail.com>
2019-09-08 18:52:07 +0300

    add favicon

--- chunks/html/head.php
+++ chunks/html/head.php
@@ -3,6 +3,15 @@ include $_SERVER['DOCUMENT_ROOT'].'/core/config.php';
 ?>
 <head>
     <meta charset="utf-8">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/_assets/img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/_assets/img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/_assets/img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/_assets/img/favicon/site.webmanifest">
+    <link rel="mask-icon" href="/_assets/img/favicon/safari-pinned-tab.svg" color="#262626">
+    <meta name="msapplication-TileColor" content="#666666">
+    <meta name="theme-color" content="#ffffff">
+
     <title><?= $title ?></title>
     
     <script src="/_assets/module/jquery/jquery-3.4.1.min.js" charset="utf-8"></script>
