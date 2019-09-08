eae0cfa79502668364fc5c5b68ebdecb537f5bf5
Shepel Pavel <shepelpav@gmail.com>
2019-09-08 19:58:48 +0300

    add crypror

--- chunks/html/footer.php
+++ chunks/html/footer.php
@@ -1,4 +1,5 @@
 <script src="/_assets/module/translit/translit.js" charset="utf-8"></script>
+<script src="/_assets/module/cryptor/binary-cryptor-min.js" charset="utf-8"></script>
 <script src="/_assets/js/ajax.js" charset="utf-8"></script>
 
 </body>
