d6d6829cf70cee67391ed49c69a40fbff1f7ddd9
Shepel Pavel <shepelpav@gmail.com>
2019-11-18 00:33:22 +0300

    feat: добавить открытие паролей

--- public/core/fn/get_pass.php
+++ public/core/fn/get_pass.php
@@ -1,53 +1,29 @@
 <?php
 include $_SERVER['DOCUMENT_ROOT'].'/core/config.php';
-
-$connect = mysqli_connect($host, $user, $password, $database)
-or die("error");
-
-mysqli_set_charset($connect, "utf8");
-
-// получение массива значений полей пароля
-$query_pass = 'SELECT * FROM `passwd` WHERE `name` = "'.$_POST['name'].'"';
-$pass = mysqli_query($connect, $query_pass) or die("error");
-if ($pass) {
-	$pass = $pass->fetch_assoc();
-}
-
-// получение хлебных крошек
 session_start();
 
-$fldr = $_SESSION['folder']['name'];
-$pth = $_SESSION['folder']['path'];
-$bread[] = [
-	'name' => $_SESSION['folder']['name'],
-	'title' => $_SESSION['folder']['title'],
-	'path' => $_SESSION['folder']['path'],
+$backpath                           = $_SESSION['path'];
+$path                               = $_POST['name'];
+$fullpath                           = $BASEPATH.$_POST['name'];
+$file                               = $BASEPATH.$_POST['name'];
+$file_res                           = json_decode(file_get_contents($file), true);
+$tmp_with_json                      = substr($file, strrpos($file, '/')+1);
+$thisname                           = substr($tmp_with_json, 0, strrpos($tmp_with_json, '.json'));
+$breadcrumbs                        = $_SESSION['breadcrumbs'];
+$tmp_this_crumb                     = [
+    'name'                              => $thisname,
+    'link'                              => $path
 ];
-while ($fldr != 'HOME' && $pth != 'HOME' && $i < 10) {
-	$i++;
-	$query_fldr = 'SELECT * FROM `groups` WHERE `name` = "'.$pth.'"';
-	$response = mysqli_query($connect, $query_fldr) or die("error");
-	if ($response) {
-		$response = $response->fetch_assoc();
-	}
-	$fldr = $response['name'];
-	$pth = $response['path'];
-	$bread[] = $response;
-}
+array_push($breadcrumbs, $tmp_this_crumb);
 
-session_write_close();
-
-// формирование хлебных крошек
-$breadcrumbs = '';
-foreach ($bread as $crumb) {
-	if ($crumb['name'] != 'HOME' && $crumb['name'] != '') {
-		$breadcrumbs = '<div class="breadcrumbs__arrow"> > </div><div class="breadcrumbs__link js-tree-path" target="'.$crumb['name'].'">
-		'.$crumb['title'].'</div>'.$breadcrumbs;
-	}
-	$i++;
-}
-
-mysqli_close($connect);
+$_SESSION['folders_res']            = '';
+$_SESSION['backpath']               = $backpath;
+$_SESSION['path']                   = $path;
+$_SESSION['thisname']               = $thisname;
+$_SESSION['fullpath']               = $fullpath;
+$_SESSION['file']                   = $file;
+$_SESSION['file_res']               = $file_res;
+$_SESSION['breadcrumbs']            = $breadcrumbs;
 
 ?>
 
@@ -55,48 +31,58 @@ mysqli_close($connect);
 
 <div class="content">
 
-	<h2 class="js-title" this-path="<?= $pass['path'] ?>" this-fullpath="<?= $pass['fullpath'] ?>"
-		this-name="<?= $pass['name'] ?>"><?= $pass['title'] ?></h2>
+    <h2><?= $_SESSION['thisname'] ?></h2>
 
-	<div class="breadcrumbs">
-		<div class="breadcrumbs__link js-tree-path" target="HOME">HOME</div><?= $breadcrumbs ?><div
-			class="breadcrumbs__arrow"> > </div>
-		<div class="breadcrumbs__nolink"><?= $pass['title'] ?></div>
-	</div>
+    <div class="breadcrumbs">
+        <?php if ($_SESSION['path'] != '') {
+        echo '<div class="breadcrumbs__link js-tree-path" target="HOME">HOME</div>';
+        foreach ($_SESSION['breadcrumbs'] as $crumb) {
+            if (next($_SESSION['breadcrumbs'])) {
+                echo '<div class="breadcrumbs__arrow"> > </div><div class="breadcrumbs__link js-tree-path" target="'.$crumb['link'].'">'.$crumb['name'].'</div>';
+            } else {
+                echo '<div class="breadcrumbs__arrow"> > </div><div class="breadcrumbs__nolink">'.$crumb['name'].'</div>';
+            }
+        }
+    } ?>
+    </div>
 
-	<div class="controls">
-		<div class="controls__link js-tree-path" target="<?= $pass['path'] ?>">
-			<img class="controls__link_icon" src="/_assets/img/svg/left-arrow.svg" alt="Back">
-		</div>
-		<div class="controls__link js-tree-path" target="HOME">
-			<img class="controls__link_icon" src="/_assets/img/svg/home.svg" alt="Home">
-		</div>
-	</div>
+    <?php if ($_SESSION['path'] != '') { ?>
+    <div class="controls">
+        <div class="controls__link js-tree-path" target="<?= $_SESSION['backpath'] ?>">
+            <img class="controls__link_icon" src="/_assets/img/svg/left-arrow.svg" alt="Back">
+        </div>
+        <div class="controls__link js-tree-path" target="HOME">
+            <img class="controls__link_icon" src="/_assets/img/svg/home.svg" alt="Home">
+        </div>
+    </div>
+    <?php } ?>
 
-	<div class="pass">
-		<div class="pass__input pass__input_title">
-			<input class="js-input-title" type="text" name="title" value="<?= $pass['title'] ?>">
-		</div>
-		<div class="pass__input pass__input_link">
-			<input class="js-input-link js-crypt" type="text" name="link" value="<?= $pass['link'] ?>">
-			<img class="pass__input_copy js-pass-copy" src="/_assets/img/svg/copy.svg" alt="Copy">
-		</div>
-		<div class="pass__input pass__input_login">
-			<input class="js-input-login js-crypt" type="text" name="login" value="<?= $pass['login'] ?>">
-			<img class="pass__input_copy js-pass-copy" src="/_assets/img/svg/copy.svg" alt="Copy">
-		</div>
-		<div class="pass__input pass__input_pass">
-			<input class="js-input-pass js-crypt" type="text" name="pass" value="<?= $pass['pass'] ?>">
-			<img class="pass__input_copy js-pass-copy" src="/_assets/img/svg/copy.svg" alt="Copy">
-		</div>
-		<div class="pass__input pass__input_note">
-			<textarea class="js-input-note js-crypt" name="note" rows="8" cols="80"><?= $pass['note'] ?></textarea>
-		</div>
+    <div class="pass">
+        <div class="pass__input pass__input_title">
+            <input class="js-input-title" type="text" name="title" value="<?= $_SESSION['thisname'] ?>">
+        </div>
+        <div class="pass__input pass__input_link">
+            <input class="js-input-link js-crypt" type="text" name="link" value="<?= $_SESSION['file_res']['link'] ?>">
+            <img class="pass__input_copy js-pass-copy" src="/_assets/img/svg/copy.svg" alt="Copy">
+        </div>
+        <div class="pass__input pass__input_login">
+            <input class="js-input-login js-crypt" type="text" name="login" value="<?= $_SESSION['file_res']['login'] ?>">
+            <img class="pass__input_copy js-pass-copy" src="/_assets/img/svg/copy.svg" alt="Copy">
+        </div>
+        <div class="pass__input pass__input_pass">
+            <input class="js-input-pass js-crypt" type="text" name="pass" value="<?= $_SESSION['file_res']['pass'] ?>">
+            <img class="pass__input_copy js-pass-copy" src="/_assets/img/svg/copy.svg" alt="Copy">
+        </div>
+        <div class="pass__input pass__input_note">
+            <textarea class="js-input-note js-crypt" name="note" rows="8" cols="80"><?= $_SESSION['file_res']['note'] ?></textarea>
+        </div>
 
-		<div class="pass__buttons">
-			<div class="pass__buttons_link js-pass-save">Сохранить</div>
-			<div class="pass__buttons_link js-tree-path" target="<?= $pass['path'] ?>">Отмена</div>
-		</div>
-	</div>
+        <div class="pass__buttons">
+            <div class="pass__buttons_link js-pass-save">Сохранить</div>
+            <div class="pass__buttons_link js-tree-path" target="<?= $_SESSION['backpath'] ?>">Отмена</div>
+        </div>
+    </div>
 
 </div>
+
+<?php session_write_close(); ?>
