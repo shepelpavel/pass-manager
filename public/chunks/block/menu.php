19e4a4271790a261a6dec08aa1959990de506ffc
Shepel Pavel <shepelpav@gmail.com>
2020-01-15 18:43:42 +0300

    feat: верстка генератора паролей

--- public/chunks/block/menu.php
+++ public/chunks/block/menu.php
@@ -4,32 +4,37 @@
         <span class="menu-trigger_line menu-trigger_line2"></span>
     </div>
     <div class="menu__inner">
-        <div class="button menu__inner_link js-add-group">
-            Добавить каталог
-        </div>
         <div class="button menu__inner_link js-add-pass">
             Добавить пароль
         </div>
-        <div>
-            <input class="js-generate-length" type="number" name="length" value="12" min="4" max="40">
+        <div class="button menu__inner_link js-add-group">
+            Добавить каталог
         </div>
-        <div>
-            <input class="js-generate-num" type="checkbox" name="num" checked>
-            <div>
-                Цифры
+        <div class="menu__inner_generator">
+            <div class="generator_item generator_title">
+                Генератор
             </div>
-        </div>
-        <div>
-            <input class="js-generate-sym" type="checkbox" name="sym" checked>
-            <div>
-                Спецсимволы
+            <div class="generator_item generator_length">
+                <input class="input js-generate-length" type="number" name="length" value="12" min="4" max="40">
             </div>
-        </div>
-        <div class="button menu__inner_link js-generate-button">
-            Сгенерировать
-        </div>
-        <div>
-            <pre class="js-generate-password">---</pre>
+            <div class="checkbox generator_item generator_num">
+                <input class="input js-generate-num" type="checkbox" name="num" id="checkbox_num" checked>
+                <label class="label" for="checkbox_num">
+                    Цифры
+                </label>
+            </div>
+            <div class="checkbox generator_item generator_sym">
+                <input class="input js-generate-sym" type="checkbox" name="sym" id="checkbox_sym" checked>
+                <label class="label" for="checkbox_sym">
+                    Спецсимволы
+                </label>
+            </div>
+            <div class=" button generator_item generator_button js-generate-button">
+                Сгенерировать
+            </div>
+            <div class="generator_item generator_result">
+                <pre class="result js-generate-password">---</pre>
+            </div>
         </div>
         <!-- <div class="button menu__inner_link js-change-key">
             Изменить ключ
