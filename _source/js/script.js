910440d031e13c41cd3f837a173a0f23c03d7ae9
Shepel Pavel <shepelpav@gmail.com>
2020-01-15 15:38:42 +0300

    feat: добавить генератор паролей

--- _source/js/script.js
+++ _source/js/script.js
@@ -1,5 +1,51 @@
 $(document).ready(function () {
 
+    // функция генерация пароля
+    function generatePass(lngt, mod) {
+        var result = '';
+        var template = '';
+        var letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        var numerals = '12345678901234567890123456789012345678901234567890';
+        var symbols = '!#$%&()*+-:;<=>?@[]^_{|}~!#$%&()*+-:;<=>?@[]^_{|}~';
+        switch (mod) {
+            case '0':
+                template = letters;
+                break;
+            case '1':
+                template = letters + numerals;
+                break;
+            case '2':
+                template = letters + symbols;
+                break;
+            case '3':
+                template = letters + numerals + symbols;
+                break;
+            default:
+                template = letters + numerals;
+        }
+        var max_position = template.length - 1;
+        for (i = 0; i < lngt; ++i) {
+            position = Math.floor(Math.random() * max_position);
+            result = result + template.substring(position, position + 1);
+        }
+        return result;
+    }
+
+    // генерация пароля
+    $('body').on('click', '.js-generate-button', function () {
+        var lngt = $('.js-generate-length').val();
+        if ($('.js-generate-num').prop('checked') && $('.js-generate-sym').prop('checked')) {
+            var mod = '3';
+        } else if ($('.js-generate-sym').prop('checked')) {
+            var mod = '2';
+        } else if ($('.js-generate-num').prop('checked')) {
+            var mod = '1';
+        } else {
+            var mod = '0';
+        }
+        $('.js-generate-password').text(generatePass(lngt, mod));
+    });
+
     // открытие меню
     $('body').on('click', '.js-menu-trigger', function () {
         $(this).toggleClass('open');
--- public/chunks/block/menu.php
+++ public/chunks/block/menu.php
@@ -10,6 +10,27 @@
         <div class="button menu__inner_link js-add-pass">
             Добавить пароль
         </div>
+        <div>
+            <input class="js-generate-length" type="number" name="length" value="12" min="4" max="40">
+        </div>
+        <div>
+            <input class="js-generate-num" type="checkbox" name="num" checked>
+            <div>
+                Цифры
+            </div>
+        </div>
+        <div>
+            <input class="js-generate-sym" type="checkbox" name="sym" checked>
+            <div>
+                Спецсимволы
+            </div>
+        </div>
+        <div class="button menu__inner_link js-generate-button">
+            Сгенерировать
+        </div>
+        <div>
+            <pre class="js-generate-password">---</pre>
+        </div>
         <!-- <div class="button menu__inner_link js-change-key">
             Изменить ключ
         </div> -->
