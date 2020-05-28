b5a831b08e15d7a79cca8c0ab83bf7ad8c2ac15c
Shepel Pavel <shepelpav@gmail.com>
2020-05-28 12:28:15 +0300

    del print_r

--- public/core/config.php
+++ public/core/config.php
@@ -2,7 +2,6 @@
 
 // data
 $BASEPATH       = substr($_SERVER['DOCUMENT_ROOT'], 0, strrpos($_SERVER['DOCUMENT_ROOT'], '/')) . '/data/' . $_SERVER['PHP_AUTH_USER'] . '/';
-echo '<pre>'; print_r($BASEPATH); echo '</pre>';
 
 // meta
 $title          = 'PssMgr';
