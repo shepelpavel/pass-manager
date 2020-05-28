3a7feef58e41015d33a2be3a3356df4cefa4651e
Shepel Pavel <shepelpav@gmail.com>
2020-05-28 12:21:21 +0300

    more user

--- public/core/config.php
+++ public/core/config.php
@@ -1,10 +1,11 @@
 <?php
 
 // data
-$BASEPATH       = substr($_SERVER['DOCUMENT_ROOT'], 0, strrpos($_SERVER['DOCUMENT_ROOT'], '/')) . '/data';
+$BASEPATH       = substr($_SERVER['DOCUMENT_ROOT'], 0, strrpos($_SERVER['DOCUMENT_ROOT'], '/')) . '/data/' . $_SERVER['PHP_AUTH_USER'] . '/';
+echo '<pre>'; print_r($BASEPATH); echo '</pre>';
 
 // meta
 $title          = 'PssMgr';
 
 // version
-$ver            = '?v=0.1.5';
+$ver            = '?v=0.1.6';
